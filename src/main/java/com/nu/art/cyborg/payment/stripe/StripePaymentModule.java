e0fa1fae62acc6ba4deafc67d6d5108ccc339d83
TacB0sS <adam.vdk@nu-art-software.com>
2019-03-19 23:01:38 +0200

    migrated without history...

---
+++ src/main/java/com/nu/art/cyborg/payment/stripe/StripePaymentModule.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2017 to Adam van der Kruk (Zehavi) AKA TacB0sS - Nu-Art Software
+ *
+ * This software code is not an 'Open Source'!
+ * In order to use this code you MUST have a proper license.
+ * In order to obtain a licence please contact me directly.
+ *
+ * Email: Adam.Zehavi@Nu-Art-Software.com
+ */
+
+package com.nu.art.cyborg.payment.stripe;
+
+import com.nu.art.cyborg.payment.PaymentModule;
+import com.nu.art.core.exceptions.runtime.BadImplementationException;
+import com.nu.art.cyborg.annotations.ModuleDescriptor;
+import com.stripe.android.Stripe;
+import com.stripe.exception.AuthenticationException;
+
+/**
+ * Created by TacB0sS on 22-Aug 2016.
+ */
+
+@ModuleDescriptor
+public class StripePaymentModule
+	extends PaymentModule {
+
+	private String stripeKey;
+	private Stripe stripe;
+
+	@Override
+	protected void init() {
+		try {
+			stripe = new Stripe(stripeKey);
+		} catch (AuthenticationException e) {
+			throw new BadImplementationException(e.getMessage() + " Stripe manager failed to initialized...");
+		}
+		logDebug("Strip successfully authenticated...");
+	}
+
+	public void setStripeKey(String stripeKey) {
+		this.stripeKey = stripeKey;
+	}
+}
